ad9b8cfd994e70ef16f5cda414ac8d34df64f670
Gabrielfonseca1008 <gfcruz06@gmail.com>
2026-02-19 21:06:16 -0300

    Adição calcular diarias

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World! This is a simple JavaScript file.");
+let custoDeVida = []
+
+function calculateDiaries() {
+    let cdv = custoDeVida
+    let pretensao = cdv * 2
+    let diariaLivre = pretensao / 20
+    console.log(`Sua diária livre ideal é de R$${diariaLivre.toFixed(2)}`)
+}
+
