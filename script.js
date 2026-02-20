653e2a690ba6dd16bc4f4afa79f091459c8bfc37
Gabrielfonseca1008 <gfcruz06@gmail.com>
2026-02-19 22:30:48 -0300

    Adição parâmetro deslocamento

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-let diasEstimados = 15
-
-
-let custoDeVida = 1800.00
 
 //Para calcular a diária livre ideal
-function calcularDiarias() {
-    let cdv = custoDeVida
-    let pretensao = (cdv * 2)
+function calcularDiarias(custoDeVida) {
+    let pretensao = (custoDeVida * 2)
     let diariaLivre = pretensao / 22
     console.log(`Sua diária livre ideal é de R$${diariaLivre.toFixed(2)}`)
 }
 
 
 //Para calcular o valor gasto com transporte
-function calcularTransporte() {
+function calcularTransporte(diasEstimados, deslocamento) {
+
     let tarifasTransporte = {
-        "Uber": 20.00,
-        "Onibus": 5.70,
-        "Carro": 10.00
+        Uber: 10.00,
+        Onibus: 5.70,
+        Carro: 24.00
     }
 
-    let desolcamento = ["Uber", "Onibus", "Carro"]
-    for(let i=0 ; i<desolcamento.length ; i++) {
-        if(desolcamento[i] === "Uber") {
+    if(deslocamento === "Uber") {
             let valorTarifa = tarifasTransporte["Uber"]
-            let transporteUber = valorTarifa * diasEstimados
+            let transporteUber = valorTarifa * diasEstimados * 2
             console.log(`O valor gasto com Uber será de R$${transporteUber.toFixed(2)}`)
-        } else if(desolcamento[i] === "Onibus") {
+        } else if(deslocamento === "Onibus") {
             let valorTarifa = tarifasTransporte["Onibus"]
-            let transporteOnibus = valorTarifa * diasEstimados
+            let transporteOnibus = valorTarifa * diasEstimados * 2
             console.log(`O valor gasto com Onibus será de R$${transporteOnibus.toFixed(2)}`)
-        } else if(desolcamento[i] === "Carro") {
+        } else if(deslocamento === "Carro") {
             let valorTarifa = tarifasTransporte["Carro"]
-            let transporteCarro = valorTarifa * diasEstimados
+            let transporteCarro = valorTarifa * diasEstimados * 2
             console.log(`O valor gasto com Carro será de R$${transporteCarro.toFixed(2)}`)
+        } else {
+            console.log("Tipo de deslocamento inválido.")
         }
     }
-}
+
+calcularDiarias(1800.00)
+calcularTransporte(20, "Onibus")
 
